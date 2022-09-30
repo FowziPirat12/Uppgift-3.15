994e07b18fae3e4df3b96b440809f893f047ce14
FowziPirat12 <fowzino16@gmail.com>
2022-09-30 11:12:46 +0200

    Add project files.

---
+++ Uppgift 3.15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Uppgift_3._15
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar ska du hyra bilen?");
+            string tid = Console.ReadLine();
+            Console.WriteLine("Hur mycket kommer det att kosta?");
+           string pris = Console.ReadLine();    
+            
+            float tid_tal1 = float.Parse(Console.ReadLine());
+            float pris_tal1 = float.Parse(Console.ReadLine());
+
+            float dagar = 80 * tid_tal1; 
+        }
+    }
+}
